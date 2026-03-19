91a7e6fd883cf536ddc52ddf256f8620f7ebebc7
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-03-19 11:52:33 +0100

    add handleClick and randomRes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
+import { useState } from "react"
 
 const Responses = ['pierre', 'feuille', 'ciseaux']
 
-export default function App(){
+function randomResponse() {
+  let randomRes = Math.floor(Math.random() * (2 - 0 + 1)) + 0
+  console.log(randomRes)
+}
+
+export default function App() {
+
+  function handleClick(index) {
+
+    if (index === 0) {
+      console.log(Responses[0])
+
+    } else if (index === 1) {
+      console.log(Responses[1])
+
+    } else if (index === 2) {
+      console.log(Responses[2])
+    }
+  }
 
   return (
     <>
       <div>
         <h1>Shifumi</h1>
-
         <div>
-
+          {Responses.map((Response, index) => (
+            <button key={index} onClick={() => handleClick(index, randomResponse())}>
+              {Response}
+            </button>
+          ))}
         </div>
       </div>
     </>
