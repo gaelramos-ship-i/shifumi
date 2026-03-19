0a0a0e498c4f6e25f6bbcc2a1e19b72122b276dd
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-03-19 15:28:38 +0100

    add getWinner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,21 @@ import { useState } from "react"
 
 const Responses = ['pierre', 'feuille', 'ciseaux']
 
+function getWinner(player, ordi) {
+  let result = ''
+
+  if(player === ordi){
+    result = 'Egalité'
+    console.log(result)
+  } else if((player === 0 && ordi === 2) || (player === 2 && ordi === 1) || (player === 1) && (ordi === 0)){
+    result = 'Gagné'
+    console.log(result)
+  } else {
+    result = 'perdu'
+    console.log(result)
+  }
+}
+
 function randomResponse() {
   return Math.floor(Math.random() * (2 - 0 + 1)) + 0
 }
@@ -15,6 +30,8 @@ export default function App() {
 
     setPlayer(Responses[index])
     setOrdinateur(Responses[random])
+
+    getWinner(index, random)
   }
 
   return (
